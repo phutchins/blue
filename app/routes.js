569dc8ba7f3f59b08e2e6fd1a2974b7b3bacfcec
Philip Hutchins <philip@bitpay.com>
2015-04-19 21:43:55 -0400

    working on order saving, some bugs here

--- app/routes.js
+++ app/routes.js
@@ -72,6 +72,7 @@ module.exports = function(app, passport) {
   // Projects View
   app.get('/projects', isLoggedIn, function(req, res) {
     Project.find().populate('_owner').exec( function(err, projects) {
+      console.log("(GET) - /projects - Rendering index.ejs");
       res.render('projects/index.ejs', {
         user : req.user,
         projects : projects
@@ -95,21 +96,22 @@ module.exports = function(app, passport) {
           });
         } else {
           defaultBoardId = project.membership._defaultBoard._id;
-          console.log("defaultBoardId is '" + defaultBoardId + "'");
+          //console.log("defaultBoardId is '" + defaultBoardId + "'");
         };
 
-        console.log("Project (GET): boards - ",project.membership._boards);
+        //console.log("Project (GET): boards - ",project.membership._boards);
 
         // Maybe do (project.membership._boards, { path: '_columns._cards'}...
         Column.populate(project, { path: 'membership._boards._columns' }, function(err, projectPopulatedColumns) {
           Card.populate(projectPopulatedColumns, { path: 'membership._boards._columns._cards' }, function(err, projectPopulatedCards) {
             if (err) { return console.log(err); }
 
-            console.log("projectPopulatedCards: ", projectPopulatedCards);
+            //console.log("projectPopulatedCards: ", projectPopulatedCards);
             var boards = projectPopulatedCards.membership._boards.toObject();
             boards.sort(function(b1, b2) { return b1._id - b2._id; });
 
             req.user.session.lastProject = projectPopulatedCards.name;
+            console.log("(GET) - /projects/:projectName - Rendering project.ejs");
             res.render('projects/project.ejs', {
               user : req.user,
               boards : projectPopulatedCards.membership._boards,
@@ -119,6 +121,7 @@ module.exports = function(app, passport) {
           });
         });
       } else {
+        console.log("(GET) - /orjects/:projectName - Rendering project.ejs without boards");
         res.render('projects/project.ejs', {
           user : req.user,
           project: project
@@ -268,7 +271,7 @@ module.exports = function(app, passport) {
         if (err) res.writeHead(500, err.message)
         else if (card == null) res.writeHead(404);
         else {
-          console.log("/cards/" + req.param('cardId') + " - Found card '" + cardPopulated.name + "'");
+          console.log("(GET) /cards/" + req.param('cardId') + " - Found card '" + cardPopulated.name + "'");
           console.log("(GET) /cards/ - Returning card json - ", cardPopulated);
           res.json({ data: JSON.stringify(cardPopulated) });
         }
@@ -278,17 +281,51 @@ module.exports = function(app, passport) {
 
   // Update column card order
   app.post('/column/order', isLoggedIn, function(req, res) {
-
+    var columnId = req.body.columnId;
+    var cardOrder = req.body.cardOrder;
+    console.log("Updating column ID '"+columnId+"' with order: '"+cardOrder+"'");
+    //if (cardOrder[0] == null) {
+    //  console.log("Sort order request submitted empty request");
+    //  res.status(404, "empty request").end();
+    //} else {
+    Column.findOne({ _id: columnId }).populate("_cards").exec( function(err, column) {
+      if (err) {
+        console.log("Error finding column with id: "+columnId+" err: "+err);
+        res.status(500, "error").end();
+      };
+      if (column == null) {
+        console.log("No column found with id: "+columnId);
+        res.status(404, "failure").end();
+      } else {
+        console.log("Found colum '"+column.name);
+        //console.log("Column _cards before update: "+column._cards);
+        var cardOrderArray = cardOrder.split(',');
+        var cardOrderObjects = [];
+        var cardCount = (cardOrderArray.length - 1);
+        var count = 0;
+        // This might should check for null also
+        if (cardOrderArray.length >= 1) {
+          console.log("cardOrderArray: "+cardOrderArray);
+          cardOrderArray.forEach( function(cardId) {
+            console.log("Sort order processing card: "+cardId);
+            cardOrderObjects[count] = mongoose.Types.ObjectId(cardId);
+            count = count + 1;
+          });
+        }
+        column._cards = cardOrderArray;
+        column.save( function(err) {
+          console.log("Column _cards after update: "+column._cards);
+          res.status(200, "success").end();
+        });
+      };
+    });
   });
 
   // Move a card to a different column
   app.post('/card/move', isLoggedIn, function(req, res) {
     var newColumnId = req.body.newColumnId;
     var cardId = req.body.cardId;
-    var cardOrder = req.body.cardOrder;
     console.log("Move Card (POST): cardId: " + req.body.cardId + " newColumnId: " + req.body.newColumnId);
-    // We can accept either cardId and newColumnId or cardOrder and columnId
-    // This should be broken out into two functions, updateColumnOrder and updateCardMembership
     Card.findOne( { _id: cardId }).populate('membership._column').exec( function (err, card) {
       // If no card is found send a 404 back and stop processing the request
       if (card == null) {
@@ -300,6 +337,7 @@ module.exports = function(app, passport) {
         console.log("oldColumn._id: "+oldColumn._id+" newColumnId: "+newColumnId);
         if (oldColumn._id == newColumnId) {
           console.log("Card moved within column");
+          res.status(200, "success").end();
         } else {
           console.log("(POST) /card/move [INFO] - oldColumn name: " + oldColumn.name);
           console.log("Move Card (POST): Found card '", card.name, "' with id '",card._id,"'");
